1cc489dbf04080708dea55addab3d1e49f7448e4
Merlin <merlin31770@gmail.com>
2022-03-25 17:44:43 +0100

    Ajout de plusieurs films

--- save_film.php
+++ save_film.php
@@ -1,21 +1,25 @@
 <?php
 
-$film = htmlspecialchars($_POST['film']);
+$film =$_POST['film'];
+$date = date('Y-M-d');
 // On se connecte à la base
-$bdd = new PDO('mysql:host=localhost;dbname=CinePS','root','');
+$bdd = new PDO('mysql:host=localhost;dbname=cineps','root','');
 // On écrit la requête
- $sql= "INSERT INTO `film`(`titre`) VALUES (".$film.")";
+ $sql= "INSERT INTO `film`(`titre`,`date`) VALUES (?, ?)";
 
 // On prépare la requête
 $query = $bdd->prepare($sql);
 
 // On exécute la requête
-if(!$query->execute(array($film))){
+if(!$query->execute([$film, $date]))
+{
     die("Une erreur est survenue");
-};
-
+}
+echo "$film a été ajouté";
 //On recupère l'id
 /*$id = $dbb->lastInsertId();
 die("Le film est ajouté sous le numéro $id");*/
 
 ?>
+<a href="propose_film.php"><button>Encore des films à proposer ?</button></a>
+<a href="index.php"><button>Voir la liste</button></a>
