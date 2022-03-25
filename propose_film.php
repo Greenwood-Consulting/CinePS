e8c52f2a3329394bd5df736bdbc2ea8b820f84dc
Merlin <merlin31770@gmail.com>
2022-03-25 17:41:50 +0100

    Ajout de plusieurs film

---
+++ propose_film.php
@@ -0,0 +1,19 @@
+<?php
+//On salut le membre
+echo "Bonjour ".$_POST['user'];
+
+?>
+
+<form method="POST" action="save_film.php">
+    <!--label> Proposition de films:</label>
+    <input type="text" name="film">
+    <label> Proposition de films:</label>
+    <input type="text" name="film">
+    <label> Proposition de films:</label>
+    <input type="text" name="film">
+    <label> Proposition de films:</label>
+    <input type="text" name="film"-->
+    <label> Proposition de films:</label>
+    <input type="text" name="film">
+    <button type="submit">Ajouter un film</button>
+</form>
