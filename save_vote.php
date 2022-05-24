72ad53ba5943b104238b5ea4accf3c697f2adebe
Merlin <merlin31770@gmail.com>
2022-05-24 17:45:23 +0200

    ajout des modifications des infos de la bdd

--- save_vote.php
+++ save_vote.php
@@ -8,9 +8,10 @@ echo $_POST['1'];
 foreach($_POST as $film_id=>$film_vote){
     $bdd = new PDO('mysql:host=localhost;dbname=cineps','root','');
     $requete = $bdd->query('SELECT * FROM film WHERE id='.$film_id);
-    $current_film = $requete->fetch();
+    $requete1 = $bdd->query('SELECT * FROM proposition');
+    $current_film = $requete1->fetch();
     $new_score= $current_film['score'] - $film_vote;
-    $requete = $bdd->query('UPDATE film set score='.$new_score.' WHERE   id='.$film_id);
+    $requete = $bdd->query('UPDATE proposition set score='.$new_score.' WHERE   id='.$film_id);
     $requete->fetch();
 
 }
