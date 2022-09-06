02f1a36f25eeecd4d202bca28c3e7bdbe1585653
Merlin <merlin31770@gmail.com>
2022-09-06 13:39:19 +0200

    Suppression du bug lors du vote

--- save_vote.php
+++ save_vote.php
@@ -4,6 +4,13 @@ include('common.php');
 
 $bdd = new PDO('mysql:host=localhost;dbname=cineps','root','');
 
+
+// Mise à jour de la table a_vote pour l'utilisateur connecté
+$user= $bdd->query("SELECT id FROM membre WHERE Prenom = '".$_SESSION['user']. "'");
+$id_utilisateur_connecte = $user->fetch()['id'];
+$insert_a_vote = $bdd->query("INSERT INTO `a_vote` (`votant`, `semaine`) VALUES ('".$id_utilisateur_connecte."','".$id_current_semaine."')");
+
+
 if(!isset($_POST['abstention'])){//si on appui sur le bouton "proposition terminée" ça va le mettre dans la bdd et un message s'affichera sur la fenetre
     foreach($_POST as $proposition_id=>$film_vote){// Mise à jour des scroes de tous les films
         $get_proposition = $bdd->query("SELECT * FROM proposition WHERE id = '".$proposition_id."'");
@@ -12,18 +19,15 @@ if(!isset($_POST['abstention'])){//si on appui sur le bouton "proposition termin
         $update_proposition = $bdd->query('UPDATE proposition SET score='.$new_score.' WHERE id='.$proposition_id);
         $update_proposition->fetch();
         // Sauvegarder le vote de la personne
-        $insert_vote = $bdd->query("INSERT INTO `votes` (`semaine`, `membre`, `proposition`, `vote`) VALUES ('".$id_current_semaine."', '".$id_utlisateur_connecte."', '".$proposition_id."', '".$film_vote."')");
-    }
+        $insert_vote = $bdd->query("INSERT INTO `votes` (`semaine`, `membre`, `proposition`, `vote`) VALUES ('".$id_current_semaine."', '".$id_utilisateur_connecte."', '".$proposition_id."', '".$film_vote."')");
   }
+}
 
 
 
-// Mise à jour de la table a_vote pour l'utilisateur connecté
-$user= $bdd->query("SELECT id FROM membre WHERE Prenom = '".$_SESSION['user']. "'");
-$id_utlisateur_connecte = $user->fetch()['id'];
-$insert_a_vote = $bdd->query("INSERT INTO `a_vote` (`votant`, `semaine`) VALUES ('".$id_utlisateur_connecte."','".$id_current_semaine."')");
 
 
+echo 'utilisateur connecte' .$id_utilisateur_connecte. '<br/>' ;
 
 echo 'Votre vote a été enregistré ! <a href=index.php><button>Revenir</button>';
 
