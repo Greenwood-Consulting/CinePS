6e459b38ac497bdc161a17980bb960d57bcf8263
Merlin <merlin31770@gmail.com>
2022-09-12 17:56:41 +0200

    Page des resultat vote avec son css

---
+++ resultat_vote.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="resultat_vote.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include('common.php');
+echo "<h1>Résultat du vote</h1>";
+echo "<a href=index.php><button type='button' class='btn btn-warning'>Revenir</button></a>";
+printChoixvote($id_current_semaine);
+?>
+<p> xr </p>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+a <br/>
+
+</body>
+</html>
+
+
