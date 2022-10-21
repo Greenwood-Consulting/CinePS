da1d3c5d90a3cd23715b7ffcc5fd28a909ca179d
Merlin <merlin31770@gmail.com>
2022-10-21 16:38:16 +0200

    condition d'affichage de la page historique film

--- historique_film.php
+++ historique_film.php
@@ -9,8 +9,9 @@
   <meta name="google" content="notranslate" />
   <meta content="Mashup templates have been developped by Orson.io team" name="author">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="historique_film.css" rel="stylesheet">
 
-  <!-- Disable tap highlight on IE-->
+  <!--Disable tap highlight on IE-->
   <meta name="msapplication-tap-highlight" content="no">
   
   <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-icon-180x180.png">
@@ -31,20 +32,25 @@
         <div class="col-xs-12">
           <div class="hero-full-wrapper">
             <div class="text-content"-->
-              <h1 class="text-warning">Historique des propositions</h1>
+              <h1 id = 'titre'>Historique des propositions</h1>
 <?php
 
 echo "<a href='index.php'><button type='button' class='btn btn-warning'>Page d'accueil</button></a>";
 include('common.php');
+include('calcul_etat.php');
   $requete8 = $bdd->query("SELECT id, jour, proposeur FROM semaine");
   while ($semaine = $requete8->fetch()){
     $id_semaine = $semaine['id'];
     $jour_semaine = $semaine['jour'];
     $proposeur_semaine = $semaine['proposeur'];
-    echo " <h2<> Les propositions de ".$proposeur_semaine;
+    echo " <h2 > Les propositions de ".$proposeur_semaine;
     echo " Pour la semaine du ".$jour_semaine. "</h2><br/>";
-    printVotesSemaine($id_semaine);
-}
+    if(($id_semaine == $id_current_semaine) && !$vote_termine_cette_semaine){//Si c'est la semaine en cours et que le vote n'est pas terminé
+      echo "Le vote n'est pas terminé, vous ne pouvez pas voir le résultat";  
+    }else{
+      printVotesSemaine($id_semaine);
+    }
+  }
 
 ?>
   </div>
