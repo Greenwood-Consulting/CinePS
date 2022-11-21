9428b3a0fca9c0d90bf304fc26cc01fbfb0a5667
Merlin <merlin31770@gmail.com>
2022-11-21 18:33:52 +0100

    Nouveau graphique avec le nombre de proposeurs

--- stat_barre.php
+++ stat_barre.php
@@ -1,6 +1,7 @@
 <?php
 include('common.php');
 $bdd = new PDO('mysql:host=localhost;dbname=cineps','root','');
+//Construction du tableau data_score
 $data_score = [];
 $get_film_semaine= $bdd->query("SELECT film, score  FROM proposition WHERE semaine = '".$id_current_semaine."'");
 
@@ -11,6 +12,22 @@ while($film_semaine = $get_film_semaine->fetch()){
 }
 
 $count_data_score = count($data_score);
+
+
+
+//construction du tableau data_proposeur
+$data_proposeurs = [];
+$get_proposeurs = $bdd->query("SELECT proposeur, COUNT(id) AS nb_proposeurs FROM semaine GROUP BY proposeur");
+
+while($proposeurs = $get_proposeurs->fetch()){
+  echo "proposeur". $proposeurs['proposeur'];
+  echo "<br/>";
+  echo "nb_proposeurs". $proposeurs['nb_proposeurs'];
+  echo "<br/>";
+  array_push($data_proposeurs, array("Proposeur" => $proposeurs['proposeur'], "nombre" => $proposeurs['nb_proposeurs']));
+}
+
+$count_data_proposeurs = count($data_proposeurs);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,6 +44,7 @@ $count_data_score = count($data_score);
     google.charts.setOnLoadCallback(drawMaterial);
 
     function drawMaterial() {
+      //draw data_score
       var data_score = new google.visualization.DataTable();
       data_score.addColumn('string', 'Film');
       data_score.addColumn('number', 'Score');
@@ -53,12 +71,42 @@ $count_data_score = count($data_score);
         bars: 'horizontal'
       };
       var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
-      materialChart.draw(data_score, materialOptions);      
+      materialChart.draw(data_score, materialOptions);
+      
+      //draw data_proposeurs
+      var data_proposeurs = new google.visualization.DataTable();
+      data_proposeurs.addColumn('string', 'proposeurs');
+      data_proposeurs.addColumn('number', 'nombre');
+
+      data_proposeurs.addRows([
+        <?php
+          for($i=0;$i<$count_data_proposeurs;$i++){
+            echo "['" . $data_proposeurs[$i]['Proposeur'] . "'," . $data_proposeurs[$i]['nombre'] . "],";
+          } 
+        ?>
+      ]);
+
+      var materialOptions = {
+        chart: {
+          title: 'nombre de propositions'
+        },
+        hAxis: {
+          title: 'nombre',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'proposeurs'
+        },
+        bars: 'horizontal'
+      };
+      var materialChart = new google.charts.Bar(document.getElementById('chart_proposeurs'));
+      materialChart.draw(data_proposeurs, materialOptions);
     }
 </script>
 </head>
 <body>
     <h2>Graphique</h2>
     <div id="chart_div"></div>
+    <div id="chart_proposeurs"></div>
 </body>
 </html>
