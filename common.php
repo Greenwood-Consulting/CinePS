69b49198aa982e353a4ee4c9c4f28f7aeeb4b350
Merlin <merlin31770@gmail.com>
2024-01-12 18:17:54 +0100

    Ajout de la fonction qui créer la colonne 'note', le formulaire pour pouvoir noter ainsi que la moyenne des notes attribuées

--- common.php
+++ common.php
@@ -79,6 +79,7 @@ function printChoixvote($id_semaine){
   // récupération des propositions pour tester s'il a des propositions
   $get_propositions = callAPI("/api/filmsProposes/".$id_semaine);
   $propositions_array = json_decode($get_propositions);
+ 
 
   if(count($propositions_array)==0){
     echo "<p><b>Pas de proposition pour cette semaine</b> </p><br/>";
@@ -91,8 +92,9 @@ function printChoixvote($id_semaine){
     $get_propositions_et_votes = callAPI("/api/votes/".$id_semaine);
     $array_propositions_et_votes = json_decode($get_propositions_et_votes);
 
-    echo "<TABLE border = '1px'>";
 
+    echo "<TABLE border = '1px'>";
+   
     // Affichage du header du tableau :
     echo "<TR>";
     echo "<TD></TD><TD></TD>";
@@ -106,7 +108,13 @@ function printChoixvote($id_semaine){
     echo "<TD>";
     echo "Score";
     echo "</TD>";
+    echo "<TD>";
+    echo "Note";
+    echo "</TD>";
     echo "</TR>";
+
+  
+
     // Fin affichage header
     
     // Affichage du corps du tableau :
@@ -117,11 +125,15 @@ function printChoixvote($id_semaine){
       echo '<TD><a class="text-dark" href = '.$proposition_et_votes->film->imdb.'>' .$proposition_et_votes->film->titre.' </a></TD>';
       echo '<TD> '.$proposition_et_votes->film->sortie_film.'</TD>';
 
+      // Ajoutez une variable pour stocker la somme des notes
+      $sumOfNotes = 0;
+
       foreach($proposition_et_votes->vote as $vote){
         if($vote->membre != $proposeur_prenom){
           echo "<TD>";
           echo $vote->vote;
           echo "</TD>";
+
         }
       }
 
@@ -129,11 +141,67 @@ function printChoixvote($id_semaine){
       echo "<TD>";
       echo $proposition_et_votes->score;
       echo "</TD>";
+      //Colonne Note
+      echo "<TD>";
 
-      echo "</TR>";
-    }
-    echo "</TABLE>";
-  }
+      $get_film_gagnant = callAPI("/api/filmVictorieux/".$id_semaine);
+      $film_gagnant_array = json_decode($get_film_gagnant);
+
+      $id_proposition = $proposition_et_votes->id;
+
+      if($film_gagnant_array[0]->id == $id_proposition){
+
+        // Parcourir le tableau des notes et calcul de la moyenne
+        $nb_notes = 0;
+        $current_user_a_note = false;
+        for ($i = 0; $i < count($array_propositions_et_votes[0]->note); $i ++)
+        {
+          if(is_int($array_propositions_et_votes[0]->note[$i]->note)){
+            if($array_propositions_et_votes[0]->note[$i]->membre == $_SESSION['user']){
+              $current_user_a_note = true;
+            }
+            $sumOfNotes= $sumOfNotes + $array_propositions_et_votes[0]->note[$i]->note;
+            $nb_notes = $nb_notes + 1;
+          }
+        }
+
+
+          // Calculer la moyenne
+          if($nb_notes !== 0){
+            $moyenne = $sumOfNotes/ $nb_notes;
+          }
+
+
+          if(!$current_user_a_note){
+          echo "<form method='POST' action='save_note.php'>";
+
+          echo '<select name="note" id="'.$id_proposition.'">';
+          echo '<option value="1">1</option>';
+          echo '<option value="2">2</option>';
+          echo '<option value="3">3</option>';
+          echo '<option value="4">4</option>';
+          echo '<option value="5">5</option>';
+          echo '<option value="6">6</option>';
+          echo '<option value="7">7</option>';
+          echo '<option value="8">8</option>';
+          echo '<option value="9">9</option>';
+          echo '<option value="10">10</option>';
+          echo '</select>';
+          echo "<button type='submit' name='id_proposition' value='".$id_proposition."'>Noter</button>";
+          echo "</TD>";
+          echo "</form>";
+
+          }else{
+            echo $moyenne;
+          }
+      }
+  
+  echo "</TR>";
 }
+
+echo "</TABLE>";
+    }
+
+  }
 
 ?>
