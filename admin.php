195c252c4f01217dcecd3ff5a25a6e5162101814
Merlin <merlin31770@gmail.com>
2024-06-04 16:28:04 +0200

    ajoue d'un style à la page admin qui reste à modifier

--- admin.php
+++ admin.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="admin.css">
     <title>Admin</title>
 </head>
 <body>
-    <h2 class="container-fluid p-5 bg-primary text-white text-center">Inscription</h2>
+    <h2>Inscription</h2>
     <?php
     include('common.php');
     if(isset($_POST['new_membre'])){//Ajout nouveau membre si on a cliqué sur le bouton d'inscription
@@ -47,7 +47,7 @@
 </br>
     
 </br>
-<h2 class="container-fluid p-5 bg-secondary text-white text-center"> Choix du proposeur pour la semaine souhaitée </h2>
+<h2> Choix du proposeur pour la semaine souhaitée </h2>
 <?php
 //si il clique sur le bouton new_proposeur, création d'une nouvelle semaine
 if(isset($_POST['new_proposeur'])){
@@ -122,8 +122,8 @@ foreach($membres as $membre) {
     echo $membre->nom;
     echo "<form method='post' action='admin.php'>";
     echo "<input type='hidden' name='id' value='$membre->id'>";
-    echo "<button type='submit' name='actif' value='1'>Activer</button>";
-    echo "<button type='submit' name='actif' value='0'>Désactiver</button>";
+    echo "<button type='submit' name='actif' value='1' class='actif'>Activer</button>";
+    echo "<button type='submit' name='actif' value='0' class='inactif'>Désactiver</button>";
     echo "</form>";
     echo "<br/><br/>";
 }
