a3c95a29d1372ebec3d79dbcdd7f82b1cf78ad00
bboisvert <bbo-github@gc2.fr>
2024-11-03 15:11:16 +0100

    fix nom du bouton statistiques

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <?php
 echo "<a href='index.php'><button type='button' class='btn btn-warning'>Accueil</button></a>";
 echo "<a href='historique_film.php'><button type='button' class='btn btn-warning'>Historique</button></a>";
-echo "<a href='stat_barre.php'><button type='button' class='btn btn-warning'>Statistique</button></a>";
+echo "<a href='stat_barre.php'><button type='button' class='btn btn-warning'>Statistiques</button></a>";
 ?>
