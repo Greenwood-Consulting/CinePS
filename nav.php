c54d295eaea7de128f3c0d5fe7934a8c9a364321
bboisvert <bbo-github@gc2.fr>
2025-03-02 17:03:27 +0100

    Barre de navitation : ajout d'un bouton 'Admin' si user id 1 est connecté

--- nav.php
+++ nav.php
@@ -2,4 +2,7 @@
 echo "<a href='index.php'><button type='button' class='btn btn-warning'>Accueil</button></a>";
 echo "<a href='historique_film.php'><button type='button' class='btn btn-warning'>Historique</button></a>";
 echo "<a href='stat_barre.php'><button type='button' class='btn btn-warning'>Statistiques</button></a>";
+if (isset($_SESSION['user']) && $_SESSION['user'] == 1 ){
+    echo "<a href='admin.php'><button type='button' class='btn btn-warning'>Admin</button></a>";
+}
 ?>
