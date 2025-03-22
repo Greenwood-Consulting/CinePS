34989372c5264f2522c96510192b4430bab3677b
bboisvert <bbo-github@gc2.fr>
2025-03-22 23:22:38 +0100

    env.php : ajout d'une constante pour stocker les id des vidéos Youtube. Page about : affichage de toutes les Vidéos Youtube

--- about.php
+++ about.php
@@ -26,15 +26,28 @@ include('call_api.php');
 
     <h1 class = 'titre'>A Propos de CinePS</h1>
 
+    <!--
     <p>
         <strong>Version du client CinePS :</strong> <?php echo "VERSION" ?><br/>
         <strong>Version de l'API CinePS :</strong> <?php echo "VERSION" ?><br/>
         <strong>Dépôts GitHub :</strong><br/>
-        <strong>Vidéos de release :</strong><br/>
-    </p>
+    </p>-->
 
     <br />
 
+    <h2>Vidéos de release</h2>
+    <div  class = "main-zone">
+
+    <?php
+    if (defined('VIDEOS_YOUTUBE') && is_array(VIDEOS_YOUTUBE)) {
+      foreach (array_reverse(VIDEOS_YOUTUBE) as $video) {
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . htmlspecialchars($video) . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>';
+      }
+    } else {
+      echo "Aucune vidéo disponible.";
+    }
+    ?>
+    </div>
 
 </div>
 </body>
