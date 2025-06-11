7773ad4f42801fb8f3723301276b4eea4037b92a
gerard <you@example.com>
2025-06-11 11:49:45 +0000

    refacto: factorisation des call_api sur /api/membres

--- auth_form.php
+++ auth_form.php
@@ -1,4 +1,5 @@
 <?php 
+require_once 'includes/helpers.php';
 
 //Si on vient du formulaire de connexion on sauvegrade l'utilisateur en session
 // si l'utilisateur se présente
@@ -8,7 +9,7 @@ if(isset($_POST['user'])){//si l'utilisateur vient du formulaire de connexion
     $id_membre = $_POST['user'];
     $password = $_POST['password'];
 
-    $json_user = call_API("/api/membres/".$id_membre, "GET");
+    $json_user = ArrayUtils::find($membres, fn($m) => $m->id == $id_membre);
 
     if(! empty($json_user)){//On vérifie qui'il y ait un mdp pour l'utilisateur connecté
         //$password = hash('sha256', $password);
@@ -25,7 +26,7 @@ if(isset($_POST['user'])){//si l'utilisateur vient du formulaire de connexion
 }
 
 if(isset($_SESSION['user'])){ //Si on est connecté on propose la déconnexion
-    $json_user = call_API("/api/membres/".$_SESSION['user'], "GET");
+    $json_user = ArrayUtils::find($membres, fn($m) => $m->id == $_SESSION['user']);
 
     ?>
     <div class="login-form">
@@ -48,8 +49,6 @@ if(isset($_SESSION['user'])){ //Si on est connecté on propose la déconnexion
     <?php
 }
 else{ //Sinon on propose la connexion
-    $json_users = call_API("/api/membres", "GET");
-
     echo "";
     echo'<form method="post" action="'.htmlspecialchars($_SERVER['REQUEST_URI']).'" class="login-form">';
     echo '<div class="form-group">
@@ -57,7 +56,7 @@ else{ //Sinon on propose la connexion
     echo '      <div class="field-group">
                     <label for="user">Membres</label>
                     <select class="text-dark" name="user" id="user">';
-                    foreach($json_users as $user){ //Afficher un utlisateur
+                    foreach($membres as $user){ //Afficher un utlisateur
                         echo"<option class='text-dark' value=".$user->id.">". $user->nom." ".$user->prenom."</option>";
                     }
     echo "           </select>
