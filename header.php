90f928194a2d907153598a68cc8c8ffe038a9ed3
gerard <you@example.com>
2025-08-11 16:05:25 +0000

    refacto: clean unused apple icon

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Greenwood Softwares">
-    <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-icon-180x180.png">
     <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
 
 <!-- La balise head est fermée dans chaque fichier php qui inclut header.php 
