da9653229f2792910f8cc162654cf0272918f1b1
gerard <you@example.com>
2025-09-23 18:21:56 +0000

    refacto: nav.php syntaxe alternative

--- nav.php
+++ nav.php
@@ -1,8 +1,6 @@
-<?php
-echo "<a href='index.php'><button type='button' class='btn btn-warning'>Accueil</button></a>";
-echo "<a href='historique_film.php'><button type='button' class='btn btn-warning'>Historique</button></a>";
-echo "<a href='stat_barre.php'><button type='button' class='btn btn-warning'>Statistiques</button></a>";
-if (isset($_SESSION['user']) && $_SESSION['user'] == 1 ){
-    echo "<a href='admin.php'><button type='button' class='btn btn-warning'>Admin</button></a>";
-}
-?>
+<a href='index.php'><button type='button' class='btn btn-warning'>Accueil</button></a>
+<a href='historique_film.php'><button type='button' class='btn btn-warning'>Historique</button></a>
+<a href='stat_barre.php'><button type='button' class='btn btn-warning'>Statistiques</button></a>
+<?php if (isset($_SESSION['user']) && $_SESSION['user'] == 1): ?>
+    <a href='admin.php'><button type='button' class='btn btn-warning'>Admin</button></a>
+<?php endif; ?>
