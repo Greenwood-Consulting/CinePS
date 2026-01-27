3bd9f95f7d98bba084610b85d14c649aaf00549a
gerard <you@example.com>
2026-01-27 02:22:43 +0000

    fix: relative path
    an url like "http://<domain>/index.php/admin.php" crashes the serveur because in this case base url is :
    http://<domain>/index.php/
    instead of:
    http://<domain>/
    
    so, all relative path used for redirection, inclusion or fetching assets are corrupted
    
    this dev replace relative paths by absolute paths

--- deconnexion.php
+++ deconnexion.php
@@ -2,7 +2,7 @@
 session_start();
 session_destroy();
 // Redirige vers la page précédente si connue, sinon vers une page par défaut
-$redirectUrl = $_SERVER['HTTP_REFERER'] ?? 'index.php';
+$redirectUrl = $_SERVER['HTTP_REFERER'] ?? '/index.php';
 header("Location: $redirectUrl");
 exit();
 ?>
--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,8 @@
 <?php
-echo "<a href='index.php'><button type='button' class='btn btn-warning'>Accueil</button></a>";
-echo "<a href='historique_film.php'><button type='button' class='btn btn-warning'>Historique</button></a>";
-echo "<a href='statistiques.php'><button type='button' class='btn btn-warning'>Statistiques</button></a>";
+echo "<a href='/index.php'><button type='button' class='btn btn-warning'>Accueil</button></a>";
+echo "<a href='/historique_film.php'><button type='button' class='btn btn-warning'>Historique</button></a>";
+echo "<a href='/statistiques.php'><button type='button' class='btn btn-warning'>Statistiques</button></a>";
 if (isset($_SESSION['user']) && $_SESSION['user'] == 1 ){
-    echo "<a href='admin.php'><button type='button' class='btn btn-warning'>Admin</button></a>";
+    echo "<a href='/admin.php'><button type='button' class='btn btn-warning'>Admin</button></a>";
 }
 ?>
