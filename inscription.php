114f65442998e49dfca4c7119cece2e4570c70b7
gerard <you@example.com>
2026-03-02 15:18:17 +0000

    fix inscription.php display

--- inscription.php
+++ inscription.php
@@ -1,30 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inscription</title>
-
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="<?= base_url('theme_signup/colorlib-regform-9/fonts/material-icon/css/material-design-iconic-font.min.css') ?>">
-
-    <!-- Main css -->
-    <link rel="stylesheet" href="<?= base_url('theme_signup/colorlib-regform-9/css/style.css') ?>">
-</head>
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=cineps','root','');
+require_once(__DIR__ . '/includes/init.php');
+
+
+// ------------- reactions au formulaires ----------------------------
+
 if(isset($_POST['new_membre'])){//Ajout nouveau membre
     $nom_de_famille = addslashes($_POST['name']);
     $prenom = addslashes($_POST['prenom']);
     $mail = addslashes($_POST['email']);
-    $ajout_membre = $bdd->prepare("INSERT INTO `membre` (`Nom`, `Prenom`, `mail`) VALUES (?,?,?)");
-    $ajout_membre->execute([$nom_de_famille, $prenom, $mail]);
-    
+
+    // @TODO: add new membre
+
+    // redirection
+    header('Location: ' . base_url('inscription.php', true, 303));
+    exit;
 }
 
+// ------------- fin des reactions au formulaires ----------------------------
 
-?>
+
+require_once(__DIR__ . '/includes/header.php'); ?>
+
+    <title>Inscription</title>
+
+    <!-- Font Icon -->
+    <!-- <link rel="stylesheet" href="<?= base_url('theme_signup/colorlib-regform-9/fonts/material-icon/css/material-design-iconic-font.min.css') ?>"> -->
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="<?= base_url('theme_signup/colorlib-regform-9/css/style.css') ?>">
+</head>
 <body>
 
     <div class="main">
@@ -52,7 +56,7 @@ if(isset($_POST['new_membre'])){//Ajout nouveau membre
     </div>
 
     <!-- JS -->
-    <script src="<?= base_url('theme_signup/colorlib-regform-9/vendor/jquery/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('theme_signup/colorlib-regform-9/js/main.js') ?>"></script>
+    <!-- <script src="<?= base_url('theme_signup/colorlib-regform-9/vendor/jquery/jquery.min.js') ?>"></script> -->
+    <!-- <script src="<?= base_url('theme_signup/colorlib-regform-9/js/main.js') ?>"></script> -->
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
