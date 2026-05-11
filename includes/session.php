126333333744b2799da2b04999388ad7b318dd3b
gerard <you@example.com>
2026-05-11 13:33:35 +0000

    fix: logout suite a la review

--- includes/session.php
+++ includes/session.php
@@ -4,6 +4,7 @@
 // https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html
 
 
+// configuration par defaut des cookies de session
 $sessionParams = [
     // Cookie session name
     'name' => SESSION_COOKIE_NAME,
@@ -21,8 +22,14 @@ $sessionParams = [
 
 // si l'utilisateur se logout
 if (isset($_POST['logout'])) {
-    // supprime la session, redirection
-    logoutSession($sessionParams);
+    // supprime la session
+    logoutSession();
+
+    // redirection (pattern POST / Redirect / GET)
+    // true => si un header Location existe déjà, remplace-le
+    // 303 => code HTTP See Other, indique au navigateur "ignore la méthode précédente et fais un GET"
+    header('Location: ' . base_url('index.php'), true, 303);
+    exit;
 }
 
 // Démarre une nouvelle session ou reprend une session existante => peuplement de $_SESSION. 
@@ -87,14 +94,27 @@ function renewSession() {
 }
 
 
-function logoutSession($params): void {
+/**
+ * Detruit la session coté serveur et le cookie de session dans le navigateur
+ */
+function logoutSession(): void {
+
+    global $sessionParams;
+
+    // Démarre une session existante => peuplement de $_SESSION. 
+    session_start($sessionParams);
+
+    // vider les variables de session en RAM serveur
+    $_SESSION = [];
+
+    // force l'envoi d'un header Set-Cookie dans la reponse
     // pour indiquer au navigateur de detruire le cookie (date d'exiration il y a ~11h)
-    $sessionParams['cookie_lifetime'] = time() - 42000;
-    // Démarre une nouvelle session ou reprend une session existante => peuplement de $_SESSION. 
-    session_start($params);
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000
+    );
+
     // supprime le fichier de session coté serveur
     session_destroy();
-    // redirection 
-    header('Location: ' . base_url('index.php'), true, 303);
-    exit;
 }
